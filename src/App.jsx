529ea182a45e0cdc9a9d1afc8dc3459b23f4e796
ConanButler <ana@butlerhq.com>
2020-10-06 19:24:06 -0600

    BG

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { Navbar, Nav } from 'react-bootstrap';
+import Home from './pages/Home'
 const App = () => {
-    return (<Switch>
-<Route exact path = '/'>
-    Hi
-</Route>
-<Route exact path = '/home'>
-    how are you 
-</Route>
-    </Switch>);
+    return (
+        <>
+            <Navbar variant="light" className="conan-nav">
+                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                <Nav className="mr-auto">
+                    <Nav.Link href="/home">Home</Nav.Link>
+                    <Nav.Link href="/contact">Contact</Nav.Link>
+                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+                </Nav>
+            </Navbar>
+                <Switch>
+
+                    <Route exact path='/'>
+                        <Redirect to='/home' />
+                    </Route>
+                    <Route exact path='/home'>
+                        <Home />
+                    </Route>
+                    <Route exact path='/contact'>
+                        hii
+                </Route>
+                </Switch>
+        </>
+    );
 };
 
 export default App;
